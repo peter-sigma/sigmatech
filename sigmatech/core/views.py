1be1202d386cce1b78c2bef4db25ce99eb70bfec
p_sigma <petergicheha39@gmail.com>
2024-09-09 15:47:36 +0300

    Fixed static files

--- sigmatech/core/views.py
+++ sigmatech/core/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    nav_items = [
+        {'name': 'Home', 'url': '/', 'icon': 'fas fa-home'},
+        {'name': 'About', 'url': '/about/', 'icon': 'fas fa-info-circle'},
+        {'name': 'Windowshop', 'url': '/shop/', 'icon': 'fas fa-shopping-cart'},
+        {'name': 'Sign In', 'url': '/signin/', 'icon': 'fas fa-sign-in-alt'},
+        {'name': 'Sign Up', 'url': '/signup/', 'icon': 'fas fa-user-plus'}
+    ]
+    return render(request, 'core/index.html', {'nav_items': nav_items})
+
 
 def about(request):
     return render(request, 'core/about.html')
