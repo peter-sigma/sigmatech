9ab24b507ce192e8bfe1a53221691fa0499572e2
p_sigma <petergicheha39@gmail.com>
2024-09-19 05:58:12 +0300

    added admin dashboard. Not yet implemented fully

--- sigmatech/dashboard/urls.py
+++ sigmatech/dashboard/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import dashboard
+from .views import *
+
+
+app_name = 'dashboard'
 
 urlpatterns = [
     path('dashboard', dashboard, name='dashboard'),
+    
+    path('admin/dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('dashboard/admin/products/', admin_manage_products, name='admin_manage_products'),
+    path('dashboard/admin/orders/', admin_manage_orders, name='admin_manage_orders'),
+    path('dashboard/admin/customers/', admin_manage_users, name='admin_manage_users'),
+    path('dashboard/admin/settings/', admin_settings, name='admin_settings'),
+    path('dashboard/admin/help/', admin_help, name='admin_help'),
 ]
--- sigmatech/dashboard/views.py
+++ sigmatech/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import user_passes_test
 
 
 # Create your views here.
@@ -11,3 +12,33 @@ def dashboard(request):
         'username': username
     }
     return render(request, 'dashboard/dashboard.html', context)
+
+
+
+# Admin restriction
+def admin_only(user):
+    return user.is_superuser
+
+@user_passes_test(admin_only)
+def admin_dashboard(request):
+    return render(request, 'dashboard/admindashboard.html')
+
+@user_passes_test(admin_only)
+def admin_manage_products(request):
+    return render(request, 'dashboard/manage_products.html')
+
+@user_passes_test(admin_only)
+def admin_manage_orders(request):
+    return render(request, 'dashboard/manage_orders.html')
+
+@user_passes_test(admin_only)
+def admin_manage_users(request):
+    return render(request, 'dashboard/manage_users.html')
+
+@user_passes_test(admin_only)
+def admin_settings(request):
+    return render(request, 'dashboard/settings.html')
+
+@user_passes_test(admin_only)
+def admin_help(request):
+    return render(request, 'dashboard/help.html')
